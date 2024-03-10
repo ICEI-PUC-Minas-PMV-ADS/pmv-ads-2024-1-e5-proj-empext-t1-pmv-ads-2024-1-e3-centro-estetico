2136c3c76603ba6b436a4f30815d3b38e048fbd1
Joao Victor Ramires <joaoramires.engsoft@gmail.com>
2024-03-09 23:20:02 -0300

    Handle Errors Globally

--- codigo-fonte/src/app.ts
+++ codigo-fonte/src/app.ts
@@ -1,6 +1,23 @@
 import fastify from 'fastify'
 import { appRoutes } from './http/routes'
+import { ZodError } from 'zod'
+import { env } from './env'
 
 export const app = fastify()
 
 app.register(appRoutes)
+
+app.setErrorHandler((error, _, reply) => {
+  if (error instanceof ZodError) {
+    reply
+      .code(400)
+      .send({ message: 'Validation Error', issues: error.format() })
+  }
+
+  if (env.NODE_ENV !== 'production') console.error(error)
+  else {
+    // TODO: Send error to monitoring service: Sentry, Datadog, NewRelic, etc
+  }
+
+  reply.code(500).send({ message: 'Internal Server Error' })
+})
