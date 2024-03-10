8c4d5f2be13154014b1ea725e064b871ecb39a7a
Joao Victor Ramires <joaoramires.engsoft@gmail.com>
2024-03-10 03:11:21 -0300

    Add specs for user register service Additionally, - Add Vitest UI

--- codigo-fonte/API/src/http/controllers/user-register.ts
+++ codigo-fonte/API/src/http/controllers/user-register.ts
@@ -1,34 +1,13 @@
-import { z } from 'zod'
 import { FastifyReply, FastifyRequest } from 'fastify'
 import { UserRegisterService } from '@/services/user-register-service'
 import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
 import { UserAlreadyExistsError } from '@/services/errors/user-already-exists-error'
+import { registerBodySchema } from '@/validations/user-validation'
 
 export async function userRegister(
   request: FastifyRequest,
   reply: FastifyReply,
 ) {
-  const MaritalStatus = z.enum(['Married', 'Single', 'Divorced'])
-  const Gender = z.enum(['Male', 'Female'])
-  const UserType = z.enum(['Admin', 'Client'])
-
-  const registerBodySchema = z.object({
-    name: z.string(),
-    birth_date: z.preprocess((arg) => {
-      if (typeof arg === 'string') {
-        return new Date(arg)
-      }
-    }, z.date()),
-    address: z.string(),
-    marital_status: MaritalStatus,
-    email: z.string().email(),
-    phone: z.string(),
-    gender: Gender,
-    additional_information: z.string().optional(),
-    user_type: UserType,
-    password: z.string().min(6),
-  })
-
   const userInputData = registerBodySchema.parse(request.body)
 
   try {
--- codigo-fonte/API/src/services/user-register-service.spec.ts
+++ codigo-fonte/API/src/services/user-register-service.spec.ts
@@ -1,5 +1,95 @@
-import { expect, test } from 'vitest'
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
+import { expect, describe, it, beforeEach, afterEach } from 'vitest'
+import {
+  UserRegisterService,
+  UserRegisterServiceType,
+} from './user-register-service'
+import { compare } from 'bcryptjs'
+import { UserAlreadyExistsError } from './errors/user-already-exists-error'
+import { ZodError } from 'zod'
 
-test('sum two numbers', () => {
-  expect(1 + 2).toBe(3)
+let userInput: UserRegisterServiceType
+let usersRepository: InMemoryUsersRepository
+let userRegisterService: UserRegisterService
+
+describe('User Register Service', async () => {
+  beforeEach(async () => {
+    usersRepository = new InMemoryUsersRepository()
+    userRegisterService = new UserRegisterService(usersRepository)
+    userInput = {
+      name: 'John Doe',
+      birth_date: new Date(),
+      address: '123 Main St',
+      marital_status: 'Single',
+      email: 'john@example.com',
+      phone: '1234567890',
+      gender: 'Male',
+      user_type: 'Client',
+      password: 'password',
+    }
+  })
+
+  // Tear down the usersRepository.items array after each test
+  afterEach(async () => {
+    usersRepository.items = []
+  })
+
+  it('should hash the user password upon registration', async () => {
+    const userData = await userRegisterService.buildUserData(userInput)
+
+    const isPasswordCorrectlyHashed = await compare(
+      'password',
+      userData.password_hash,
+    )
+
+    expect(isPasswordCorrectlyHashed).toBe(true)
+  })
+
+  it('should not register users with the same email', async () => {
+    await userRegisterService.execute(userInput)
+
+    await expect(() =>
+      userRegisterService.execute(userInput),
+    ).rejects.toBeInstanceOf(UserAlreadyExistsError)
+  })
+
+  it('should not register a new user with an invalid email', async () => {
+    const invalidUserInput = {
+      ...userInput,
+      email: 'invalid-email',
+    }
+
+    await expect(() =>
+      userRegisterService.execute(invalidUserInput),
+    ).rejects.toBeInstanceOf(ZodError)
+  })
+
+  it('should not register a new user with an invalid password', async () => {
+    const invalidUserInput = {
+      ...userInput,
+      password: '12345',
+    }
+
+    await expect(() =>
+      userRegisterService.execute(invalidUserInput),
+    ).rejects.toBeInstanceOf(ZodError)
+  })
+
+  it('should not register a new user with an invalid birth date', async () => {
+    const invalidUserInput = {
+      ...userInput,
+      birth_date: 'invalid-date',
+    }
+
+    await expect(() =>
+      // @ts-expect-error: I'm testing an invalid date
+      userRegisterService.execute(invalidUserInput),
+    ).rejects.toBeInstanceOf(ZodError)
+  })
+
+  it('should register a new user', async () => {
+    const serviceResponse = await userRegisterService.execute(userInput)
+
+    expect(serviceResponse.message).toBe('User created successfully')
+  })
 })
