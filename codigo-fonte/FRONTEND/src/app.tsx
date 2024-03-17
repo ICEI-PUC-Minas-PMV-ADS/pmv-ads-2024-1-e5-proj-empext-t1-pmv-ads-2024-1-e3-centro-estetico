b35b8eed3eb6a2b065af757ada6b7abd6f6af131
Joao Victor Ramires <joaoramires.engsoft@gmail.com>
2024-03-17 19:30:24 -0300

    Setup ShadcnUI

--- codigo-fonte/FRONTEND/src/app.tsx
+++ codigo-fonte/FRONTEND/src/app.tsx
@@ -1,7 +1,12 @@
+import { Button } from './components/ui/button'
+import './global.css'
+
 export function App() {
   return (
     <>
-      <h1>Hello, world!</h1>
+      <div className="flex justify-center items-center h-screen">
+        <Button>Enviar</Button>
+      </div>
     </>
   )
 }
