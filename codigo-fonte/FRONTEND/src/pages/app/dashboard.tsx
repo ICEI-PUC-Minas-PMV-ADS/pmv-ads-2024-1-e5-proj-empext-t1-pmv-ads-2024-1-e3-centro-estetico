409d12981bd0595d781f6726bb0c99edb64c8825
Joao Victor Ramires <joaoramires.engsoft@gmail.com>
2024-03-17 22:48:25 -0300

    Add React Helmet SEO

--- codigo-fonte/FRONTEND/src/pages/app/dashboard.tsx
+++ codigo-fonte/FRONTEND/src/pages/app/dashboard.tsx
@@ -1,7 +1,10 @@
+import { Helmet } from 'react-helmet-async'
+
 export function Dashboard() {
   return (
-    <div>
+    <>
+      <Helmet title="Dashboard" />
       <h1>Dashboard</h1>
-    </div>
+    </>
   )
 }
--- codigo-fonte/FRONTEND/src/pages/auth/sign-in.tsx
+++ codigo-fonte/FRONTEND/src/pages/auth/sign-in.tsx
@@ -1,7 +1,10 @@
+import { Helmet } from 'react-helmet-async'
+
 export function SignIn() {
   return (
-    <div>
+    <>
+      <Helmet title="Login" />
       <h1>Sign In</h1>
-    </div>
+    </>
   )
 }
