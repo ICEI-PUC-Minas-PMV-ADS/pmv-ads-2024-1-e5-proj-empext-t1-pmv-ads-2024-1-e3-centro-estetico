db02148bc8fda4ebc3e0e402fa270ae724e4e9bd
Joao Victor Ramires <joaoramires.engsoft@gmail.com>
2024-03-17 22:26:32 -0300

    Setup Eslint and Prettier

--- codigo-fonte/FRONTEND/src/app.tsx
+++ codigo-fonte/FRONTEND/src/app.tsx
@@ -1,10 +1,11 @@
-import { Button } from './components/ui/button'
 import './global.css'
+
+import { Button } from './components/ui/button'
 
 export function App() {
   return (
     <>
-      <div className="flex justify-center items-center h-screen">
+      <div className="flex h-screen items-center justify-center">
         <Button>Enviar</Button>
       </div>
     </>
