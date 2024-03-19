960550b9b3d7873c39a93e2a410bba098a9cc264
Joao Victor Ramires <joaoramires.engsoft@gmail.com>
2024-03-18 21:47:16 -0300

    Add ThemeToggle

--- codigo-fonte/FRONTEND/src/components/header.tsx
+++ codigo-fonte/FRONTEND/src/components/header.tsx
@@ -1,6 +1,7 @@
 import { Home, Syringe, User } from 'lucide-react'
 
 import { NavLink } from './nav-link'
+import { ThemeToggle } from './theme/theme-toggle'
 import { Separator } from './ui/separator'
 
 export function Header() {
@@ -21,6 +22,10 @@ export function Header() {
             Clientes
           </NavLink>
         </nav>
+
+        <div className="ml-auto flex items-center gap-2">
+          <ThemeToggle />
+        </div>
       </div>
     </div>
   )
