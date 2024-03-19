828bfcfc8f6f06ccd8da4c05da27ca80f9b41f3b
Joao Victor Ramires <joaoramires.engsoft@gmail.com>
2024-03-18 21:55:05 -0300

    Add AccountMenu Component

--- codigo-fonte/FRONTEND/src/components/header.tsx
+++ codigo-fonte/FRONTEND/src/components/header.tsx
@@ -1,5 +1,6 @@
 import { Home, Syringe, User } from 'lucide-react'
 
+import { AccountMenu } from './account-menu'
 import { NavLink } from './nav-link'
 import { ThemeToggle } from './theme/theme-toggle'
 import { Separator } from './ui/separator'
@@ -25,6 +26,7 @@ export function Header() {
 
         <div className="ml-auto flex items-center gap-2">
           <ThemeToggle />
+          <AccountMenu />
         </div>
       </div>
     </div>
