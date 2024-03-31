175d88ccaa39d83394c5399095c7b8b982964729
Joao Victor Ramires <joaoramires.engsoft@gmail.com>
2024-03-31 19:30:00 -0300

    User register

---
+++ codigo-fonte/FRONTEND/src/api/sign-up.ts
@@ -0,0 +1,17 @@
+import { api } from '@/lib/axios'
+
+export interface SignUpBody {
+  name: string
+  birth_date: Date
+  address: string
+  marital_status: 'Married' | 'Single' | 'Divorced'
+  email: string
+  phone: string
+  gender: 'Male' | 'Female'
+  additional_information?: string
+  password: string
+}
+
+export async function signUp(data: SignUpBody) {
+  await api.post('/users', { ...data, user_type: 'Client' })
+}
