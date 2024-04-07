6d32f4e8429bd7b6ade5cea32dcad40e0e2bf1b2
gisellenrosa <gisellenrosa@gmail.com>
2024-04-06 22:49:22 -0300

    feat: add validations to registerClient form

--- codigo-fonte/FRONTEND/src/pages/app/register/RegisterClients.tsx
+++ codigo-fonte/FRONTEND/src/pages/app/register/RegisterClients.tsx
@@ -1,42 +1,81 @@
 
-import { useState } from 'react';
+import { registerCLient } from '@/api/sign-up';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useMutation } from '@tanstack/react-query';
+import { Controller, useForm } from 'react-hook-form';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'sonner';
+import { z } from 'zod';
+
+
+const Gender = z.enum(['Male', 'Female'])
+
+const registerClientForm = z
+  .object({
+    name: z.string().min(3, 'Nomes precisam ter pelo menos 3 letras'),
+    birth_date: z.preprocess((arg) => {
+      if (arg instanceof Date) return arg
+      if (typeof arg === 'string') return new Date(arg)
+    }, z.date()),
+    address: z.string().regex(/^(.+),\s*(\d+),\s*(.+?)(?:,\s*(.+))?$/, {
+      message:
+        'Endereço deve seguir o padrão: Rua, número, bairro, complemento (complemento é opcional).',
+    }),
+    email: z.string().email({ message: 'Endereço de e-mail inválido.' }),
+    phone: z
+      .string()
+      .regex(
+        /^(\(\d{2}\)|\d{2})-?\s?9-?\s?\d{4,5}-?\d{4}$/,
+        'Número de telefone inválido.',
+      ),
+    gender: Gender,
+  })
+
+type RegisterClientForm = z.infer<typeof registerClientForm>
 
 export function RegisterClients() {
 
-  const [nome, setNome] = useState('');
-  const [email, setEmail] = useState('');
-  const [endereco, setEndereco] = useState('');
-  const [numero, setNumero] = useState('');
-  const [celular, setCelular] = useState('');
-  const [sexo, setSexo] = useState('');
-  const [dataNascimento, setDataNascimento] = useState('');
+  const navigate = useNavigate()
 
-  const handleSubmit = (e: any) => {
-    e.preventDefault();
+  const {
+    handleSubmit,
+    register,
+    control,
+  } = useForm<RegisterClientForm>({
+    resolver: zodResolver(registerClientForm),
+  })
 
-    // Validar dados
+  const { mutateAsync: reg } = useMutation({
+    mutationFn: registerCLient,
+  })
 
-    // Salvar dados
 
-    alert('Cliente cadastrado com sucesso!');
-  };
+  async function handleRegisterClient(data: RegisterClientForm) {
+    try {
+      await reg(data)
+
+      navigate('/sign-in')
+      toast.success('Usuário cadastrado com sucesso!')
+    } catch (error) {
+      toast.error('Erro ao cadastrar usuário!')
+    }
+  }
 
   return (
       <div className="container mx-auto">
-        <h1 className="text-center text-2xl font-bold mb-4">Cadastrar Cliente</h1>
+        <h1 className="text-center text-2xl font-bold mb-12">Cadastrar Cliente</h1>
 
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit(handleRegisterClient)}>
           <div className="mb-4">
             <label className="block text-sm font-medium" htmlFor="nome">
               Nome
             </label>
             <input
-              id="nome"
+              id="name"
               type="text"
               className="block w-full px-4 py-2 bg-transparent border-b-2 border-gray-300 outline-none text-[#00A27B]"
-              value={nome}
-              onChange={(e) => setNome(e.target.value)}
-            />
+              {...register('name')}
+              />
           </div>
           <div className="mb-4">
             <label className="block text-sm font-medium" htmlFor="email">
@@ -46,8 +85,7 @@ export function RegisterClients() {
               id="email"
               type="email"
               className="block w-full px-4 py-2 bg-transparent border-b-2 border-gray-300 outline-none  text-[#00A27B]"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
+              {...register('email')}
             />
           </div>
 
@@ -57,23 +95,10 @@ export function RegisterClients() {
                 Endereço
               </label>
               <input
-                id="endereco"
+                id="address"
                 type="text"
-                className="block w-40 px-4 py-2 bg-transparent border-b-2 border-gray-300 outline-none text-[#00A27B] "
-                value={endereco}
-                onChange={(e) => setEndereco(e.target.value)}
-              />
-            </div>
-            <div className="mb-4 ml-4">
-              <label className="block text-sm font-medium" htmlFor="numero">
-                Nº
-              </label>
-              <input
-                id="numero"
-                type="number"
-                className="block w-full px-4 py-2 bg-transparent border-b-2 border-gray-300 outline-none text-[#00A27B]"
-                value={numero}
-                onChange={(e) => setNumero(e.target.value)}
+                className="block w-full px-4 py-2 bg-transparent border-b-2 border-gray-300 outline-none text-[#00A27B] "
+                {...register('address')}
               />
             </div>
           </div>
@@ -83,39 +108,43 @@ export function RegisterClients() {
               Celular
             </label>
             <input
-              id="celular"
+              id="phone"
               type="tel"
               className="block w-full px-4 py-2 bg-transparent border-b-2 border-gray-300 outline-none text-[#00A27B]"
-              value={celular}
-              onChange={(e) => setCelular(e.target.value)}
+              {...register('phone')}
             />
           </div>
-
           <div className="mb-4">
-            <label className="block mb-2 text-sm font-medium" htmlFor="sexo">
-              Sexo
+            <label className="block mb-2 text-sm font-medium" htmlFor="gender">
+              Gênero
             </label>
             <div className="flex items-center">
-              <input
-                id="masculino"
-                type="radio"
-                name="sexo"
-                value="masculino"
-                className="mr-2 bg-transparent checked:bg-[#00A27B] ring-offset-1 h-3 w-3 rounded-full appearance-none box-border ring-2 ring-[#00A27B]"
-                checked={sexo === 'masculino'}
-                onChange={(e) => setSexo(e.target.value)}
-              />
-              <label htmlFor="masculino">Masculino</label>
+            <Controller
+            name="gender"
+            control={control}
+            render={({ field }) => (
+              <>
+                <input
+                  id="male"
+                  type="radio"
+                  className="mr-2 bg-transparent checked:bg-[#00A27B] ring-offset-1 h-3 w-3 rounded-full appearance-none box-border ring-2 ring-[#00A27B]"
+                  value="Male"
+                  onChange={(e) => field.onChange(e.target.value)}
+                  checked={field.value === "Male"}
+                />
+                <label htmlFor="male">Masculino</label>
 
-              <input
-                id="feminino"
-                type="radio"
-                name="sexo"
-                value="feminino"
-                className="ml-4 mr-2 bg-transparent checked:bg-[#00A27B] checked:ring-offset-1 h-3 w-3 rounded-full appearance-none box-border ring-2 ring-[#00A27B]"
-                checked={sexo === 'feminino'}
-                onChange={(e) => setSexo(e.target.value)}/>
-              <label htmlFor="feminino">Feminino</label>
+                <input
+                  id="female"
+                  type="radio"
+                  className="ml-4 mr-2 bg-transparent checked:bg-[#00A27B] checked:ring-offset-1 h-3 w-3 rounded-full appearance-none box-border ring-2 ring-[#00A27B]"
+                  value="Female"
+                  onChange={(e) => field.onChange(e.target.value)}
+                  checked={field.value === "Female"}                  />
+                <label htmlFor="female">Feminino</label>
+              </>
+            )}
+            />
               </div>
               </div>
 
@@ -127,8 +156,7 @@ export function RegisterClients() {
                   id="dataNascimento"
                   type="date"
                   className="block w-full px-4 py-2 bg-transparent border-b-2 border-gray-300 outline-none text-[#00A27B]"
-                  value={dataNascimento}
-                  onChange={(e) => setDataNascimento(e.target.value)}
+                  {...register('birth_date')}
                 />
               </div>
 
@@ -140,5 +168,5 @@ export function RegisterClients() {
               </button>
               </form>
           </div>
-                );
+ );
 };
