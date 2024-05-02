a4ea30a0bae4abf0d12f2edf2e69274842b36528
gisellenrosa <gisellenrosa@gmail.com>
2024-05-02 20:37:05 -0300

    fix duplicated routes

--- codigo-fonte/API/src/http/routes.ts
+++ codigo-fonte/API/src/http/routes.ts
@@ -1,16 +1,16 @@
 import { FastifyInstance } from 'fastify'
 import { authenticate } from './controllers/authenticate'
 import { clientRegister } from './controllers/client-register'
-import { userProfile } from './controllers/user-profile'
-import { userRegister } from './controllers/user-register'
 import { clients } from './controllers/clients'
 import { createAppointment } from './controllers/create-appointment'
-import { verifyJWT } from './middlewares/verify-jwt'
 import { skinForm } from './controllers/skin-form'
+import { userProfile } from './controllers/user-profile'
+import { userRegister } from './controllers/user-register'
+import { verifyJWT } from './middlewares/verify-jwt'
 
 export async function appRoutes(app: FastifyInstance) {
   app.get('/clients', clients)
-  app.post('/clients', clientRegister)
+  // app.post('/clients', clientRegister)
 
   app.post('/users', userRegister)
   app.post('/sessions', authenticate)
