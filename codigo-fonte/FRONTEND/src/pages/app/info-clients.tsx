f25afcbce2b419ee72c630ca3afb3246fd74da78
Luiz Fellipe <luizfelliipe.777@gmail.com>
2024-05-12 19:55:52 -0300

    info-clients

--- codigo-fonte/FRONTEND/src/pages/app/info-clients.tsx
+++ codigo-fonte/FRONTEND/src/pages/app/info-clients.tsx
@@ -1,5 +1,4 @@
-import { Button } from '@/components/ui/button'
-import { Phone } from 'lucide-react';
+
 import { ReactSVG } from 'react-svg';
 import imgPerson from '../../assets/imgPerson.svg'
 
@@ -16,28 +15,33 @@ function PatientInfo() {
             <div className="info">
                 <label htmlFor="email"><i className="fas fa-envelope"></i> E-mail:</label>
                 <span id="email">paciente123@gmail.com</span>
+                <button className="info-button"><i className="fas fa-edit"></i> Editar</button>
             </div>
             <div className="info">
                 <label htmlFor="genero"><i className="fas fa-venus-mars"></i> Gênero:</label>
                 <span id="genero">Masculino</span>
+                <button className="info-button"><i className="fas fa-edit"></i> Editar</button>
             </div>
             <div className="info">
                 <label htmlFor="data_nascimento"><i className="fas fa-calendar-alt"></i> Data de Nascimento:</label>
                 <span id="data_nascimento">11/05/2000</span>
+                <button className="info-button"><i className="fas fa-edit"></i> Editar</button>
             </div>
             <div className="info">
                 <label htmlFor="endereco"><i className="fas fa-map-marker-alt"></i> Endereço:</label>
                 <span id="endereco">Av. Afonso Pena, 1000 - Centro, Belo Horizonte - MG, 30130-902</span>
+                <button className="info-button"><i className="fas fa-edit"></i> Editar</button>
             </div>
-            <div className='flex justify-between'>
-                        <Button variant={'telephone'} size={'icon'}>
-                            <Phone />
-                        </Button>
-                        <p className='font-medium pt-2 pl-2'>(82) 9999-9999</p>
-                    </div>
+            <div className="info">
+        <label htmlFor="telefone"><i className="fas fa-phone"></i> Telefone:</label>
+        <span id="telefone">(82) 99999 - 9999</span>
+        <button className="info-button"><i className="fas fa-edit"></i> Editar</button>
+    </div>
         </div>
     );
 }
 
 export default PatientInfo;
+
+
 
