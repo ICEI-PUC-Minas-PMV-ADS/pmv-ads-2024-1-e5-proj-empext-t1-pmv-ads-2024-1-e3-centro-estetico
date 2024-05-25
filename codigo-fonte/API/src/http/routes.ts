8e262ea2a251a5a45d723d2a0f712dc0ff6be1c3
Joao Victor Ramires <joaoramires.engsoft@gmail.com>
2024-05-25 17:59:21 -0300

    Update routes

--- codigo-fonte/API/src/http/routes.ts
+++ codigo-fonte/API/src/http/routes.ts
@@ -3,7 +3,7 @@ import { authenticate } from "./controllers/authenticate";
 import { clientRegister } from "./controllers/client-register";
 import { clients } from "./controllers/clients";
 import { createAppointment } from "./controllers/create-appointment";
-import { createHealthQuestionnaire } from './controllers/create-health-questionnaire'
+import { createHealthQuestionnaire } from "./controllers/create-health-questionnaire";
 import { skinForm } from "./controllers/skin-form";
 import { userProfile } from "./controllers/user-profile";
 import { userRegister } from "./controllers/user-register";
@@ -20,12 +20,11 @@ export async function appRoutes(app: FastifyInstance) {
   app.post("/clients", clientRegister);
   app.post("/skin-form", skinForm);
   app.get("/user", userProfile);
-  app.get("/get-users", getUsers)
+  app.get("/get-users", getUsers);
+  app.post("/health-questionnairies", createHealthQuestionnaire);
+  app.post("/appointments", createAppointment);
 
   // Authenticated Routes
   app.get("/me", { onRequest: [verifyJWT] }, userProfile);
   // app.get('/users', { onRequest: [verifyJWT] }, users)
-  app.post("/appointments", { onRequest: [verifyJWT] }, createAppointment);
-  app.post('/health-questionnairies', createHealthQuestionnaire)
-
 }
