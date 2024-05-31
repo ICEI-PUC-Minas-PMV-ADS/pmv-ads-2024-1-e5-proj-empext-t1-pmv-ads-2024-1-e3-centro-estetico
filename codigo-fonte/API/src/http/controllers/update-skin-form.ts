b57d791525163d5fd0bc84d03780208ca862cc43
gisellenrosa <gisellenrosa@gmail.com>
2024-05-30 21:52:56 -0300

    fix: adjusts on backend to update

--- codigo-fonte/API/src/http/controllers/update-skin-form.ts
+++ codigo-fonte/API/src/http/controllers/update-skin-form.ts
@@ -1,18 +1,18 @@
 import { makeSkinFormUpdateService } from '@/services/factories/make-skin-form-update-service'
-import { getSkinFormParams, registerBodySchema } from '@/validations/skin-form-validation'
+import { getSkinFormIdParams, registerBodySchema } from '@/validations/skin-form-validation'
 import { FastifyReply, FastifyRequest } from 'fastify'
 
 export async function updateSkinForm(
   request: FastifyRequest,
   reply: FastifyReply,
 ) {
-  const params = getSkinFormParams.parse(request.query)
+  const params = getSkinFormIdParams.parse(request.query)
 
   const skinFormInputData = registerBodySchema.parse(request.body)
 
   try {
     const skinFormUpdateService = makeSkinFormUpdateService()
-    const serviceResponse = await skinFormUpdateService.execute(skinFormInputData, params.clientId)
+    const serviceResponse = await skinFormUpdateService.execute(skinFormInputData, params.skinAnalysisId)
 
     return reply.code(201).send(serviceResponse)
   } catch (error) {
--- codigo-fonte/API/src/services/factories/make-skin-form-update-service.ts
+++ codigo-fonte/API/src/services/factories/make-skin-form-update-service.ts
@@ -1,11 +1,9 @@
-import { PrismaClientsRepository } from '@/repositories/prisma/prisma-clients-repository'
 import { PrismaSkinAnalysisRepository } from '@/repositories/prisma/prisma-skin-analysis-repository'
 import { UpdateSkinFormService } from '../update-skin-form-service'
 
 export function makeSkinFormUpdateService(): UpdateSkinFormService {
   const skinAnalysisRepository = new PrismaSkinAnalysisRepository()
-  const clientsRepository = new PrismaClientsRepository()
-  const skinFormUpdateService = new UpdateSkinFormService(skinAnalysisRepository, clientsRepository)
+  const skinFormUpdateService = new UpdateSkinFormService(skinAnalysisRepository)
 
   return skinFormUpdateService
 }
